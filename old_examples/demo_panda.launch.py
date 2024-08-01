92fee8b5e5491fc22358349a1b1dc6029e060efd
Nidhish Chadive <177201926+nyne-nyne@users.noreply.github.com>
2024-08-01 12:16:30 +0530

    Getting the older example (first demo) working with Humble

--- old_examples/demo_panda.launch.py
+++ old_examples/demo_panda.launch.py
@@ -2,12 +2,12 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
 from launch_param_builder import ParameterBuilder
-
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
     moveit_ros_benchmarks_config = (
-        ParameterBuilder("moveit_ros_benchmarks")
+        ParameterBuilder("moveit_ros_benchmarks_fix")
         .yaml(
             parameter_namespace="benchmark_config",
             file_path="demo1.yaml",
@@ -17,29 +17,25 @@ def generate_launch_description():
 
     moveit_configs = MoveItConfigsBuilder("moveit_resources_panda").to_dict()
 
+    warehouse_ros_config = {
+        # For warehouse_ros_sqlite
+        "warehouse_plugin": "warehouse_ros_sqlite::DatabaseConnection",
+        "warehouse_host": "/tmp/demo1_db.sqlite",
+        "warehouse_port": 33828,
+    }
+    
     # moveit_ros_benchmark demo executable
     moveit_ros_benchmarks_node = Node(
         name="moveit_run_benchmark",
-        package="moveit_ros_benchmarks",
+        package="moveit_ros_benchmarks_fix",
         # prefix='xterm -e gdb --ex=run --args',
         executable="moveit_run_benchmark",
         output="screen",
         parameters=[
             moveit_ros_benchmarks_config,
             moveit_configs,
+            warehouse_ros_config,
         ],
     )
 
-    # Warehouse mongodb server
-    mongodb_server_node = Node(
-        package="warehouse_ros_mongo",
-        executable="mongo_wrapper_ros.py",
-        parameters=[
-            {"warehouse_port": 33829},
-            {"warehouse_host": "localhost"},
-            {"warehouse_plugin": "warehouse_ros_mongo::MongoDatabaseConnection"},
-        ],
-        output="screen",
-    )
-
-    return LaunchDescription([moveit_ros_benchmarks_node, mongodb_server_node])
+    return LaunchDescription([moveit_ros_benchmarks_node])
