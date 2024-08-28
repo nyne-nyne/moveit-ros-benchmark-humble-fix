4f3b5bed4491e0fbe5f9e76d59c0be20a690952f
Nidhish Chadive <177201926+nyne-nyne@users.noreply.github.com>
2024-08-28 15:46:40 +0530

    Taking the colours from matplotlib's colourmap. Adding a few args

--- scripts/moveit_multi_benchmark_statistics.py
+++ scripts/moveit_multi_benchmark_statistics.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-import IPython
 import argparse
-import sys
 import io
 import itertools
+import sys
 from pathlib import Path
+
+import IPython
 import matplotlib.pyplot as plt
 import numpy as np
-import xarray as xr
 import pandas as pd
+import xarray as xr
+from matplotlib import colormaps
+
 
 def skip_line(f: io.TextIOWrapper):
     f.readline()
@@ -23,6 +26,11 @@ if __name__ == "__main__":
     # run. The only thing that differs is the attempts and/or the planning time. Sometimes the planner(s)
     parser.add_argument("experiments", type=Path, nargs='+',
                         help="Path to file(s) containing benchmark logs, of the same experiment.")
+    parser.add_argument("--ishell", default=False, action="store_true",
+                        help="Run an interactive (IPython) shell at the end of the script. Defaults to 'False'")
+    parser.add_argument("--map", default="Set1",
+                        help="Colourmap to use for the plots. Defaults to 'Set1.'")
+    
     #parser.add_argument("benchmark_dirs", type=str, nargs='+',
     #                    help="Path to one or more directories that contain benchmark experiment log files.")
     # TODO, : list the files? and then look match 'em up, by sorting them?
@@ -35,11 +43,10 @@ if __name__ == "__main__":
     runs = -1
     file_name = "" # experiment_name + scene_name + extension (.pdf)
     
-    # TODO: parse the file for experiment name as well as scene name; for the file to save to
     for result_file in args.experiments:
         bm_name = ""
         with open(result_file, "r") as f:
-            print("Processing result " + str(result_file), end='', file=sys.stderr)
+            print("Processing result " + str(result_file), file=sys.stderr)
             planner_results = dict()
 
             skip_line(f)
@@ -92,7 +99,7 @@ if __name__ == "__main__":
                 data.close()
                 skip_line(f)
 
-            benchmarks[bm_name] = xr.DataArray(data=list(planner_results.values()),
+            benchmarks[bm_name] = xr.DataArray(data=list(planner_results.values()), # TODO: Add some attributes as well, from the benchmark file!
                                                dims=["planner", "run", "metric"],
                                                coords={"planner": list(planner_results.keys()),
                                                        "metric": list(metrics.keys()),
@@ -107,11 +114,16 @@ if __name__ == "__main__":
                                     "benchmark": list(benchmarks.keys())})
 
     # It's plotting time.
-    colours = ["red", "blue", "green", "cyan", "teal", "orange"] # TODO: Autogenerate these? Colour names? Or hex-values? With a nice palette, not some random colours?
+    try: 
+        colours = colormaps[args.map].colors
+    except KeyError as e:
+        print(f"Unknown colourmap name {args.map}. See https://matplotlib.org/stable/users/explain/colors/colormaps.html#choosing-colormaps-in-matplotlib. Defaulting to 'Set1'", file=sys.stderr)
+        colours = colormaps["Set1"].colors
 
     for metric, data_type in metrics.items():
         if data_type == "BOOLEAN": # bar-graph
             fig, ax = plt.subplots()
+            ax.grid(axis='y', linestyle="--", markerfacecolor="xkcd:light blue grey")
             x = np.arange(len(collected.planner))
             m = (1.0 - len(collected.benchmark))/2.0
             width = 1.0/(len(collected.benchmark) + 0.25)
@@ -128,21 +140,22 @@ if __name__ == "__main__":
                         result = 0
                     results.append(result)
 
-                bar = ax.bar(x + offset, results, width, color=colours[colour_i], label=benchmark.name)
-                ax.bar_label(bar,labels=['F' if pf else '' for pf in cfailed_planners], color=colours[colour_i])
+                bar = ax.bar(x + offset, results, width, color=colours[colour_i % len(colours)], label=benchmark.name)
+                ax.bar_label(bar,labels=['F' if pf else '' for pf in cfailed_planners], color=colours[colour_i % len(colours)])
                 m += 1.0
 
             ax.set_ylim(0, 100)
             ax.legend()
             ax.tick_params(axis='y', labelsize=10)
             ax.tick_params(axis='x', labelsize=10)
-            ax.set_xticks(x, collected.planner.data, rotation=90)
+            ax.set_xticks(x, collected.planner.data, rotation=45)
             ax.set_ylabel(f"{metric.replace('_', ' ').title()} (%)", fontsize=12)
             #ax.set_xlabel("Motion planning algorithm", fontsize=12)
             plt.show()
         else: # Uses a box-plot for all other data
             # This methodology was inspired from the answers here https://stackoverflow.com/questions/16592222/how-to-create-grouped-boxplot
             fig, ax = plt.subplots()
+            ax.grid(linestyle="--", markerfacecolor="xkcd:light blue grey")
             x = np.arange(len(collected.planner))
             m = (1.0 - len(collected.benchmark))/2.0
             width = 1/(len(collected.benchmark) + 0.25)
@@ -158,10 +171,10 @@ if __name__ == "__main__":
                     results.append(result)
 
                 boxplot = ax.boxplot(results, positions=x + offset, widths=width * 0.85, sym='+', vert=True,
-                                     flierprops=dict(markeredgecolor=colours[colour_i]), patch_artist=True, labels=['']*collected.planner.size)
+                                     flierprops=dict(markeredgecolor=colours[colour_i % len(colours)]), patch_artist=True, labels=['']*collected.planner.size)
                 # Colouring the rest of the boxplot
                 for element in boxplot.keys():
-                    plt.setp(boxplot[element], color=colours[colour_i])
+                    plt.setp(boxplot[element], color=colours[colour_i % len(colours)])
                 #plt.setp(boxplot["whiskers"], linestyle="--")
                 plt.setp(boxplot["boxes"], facecolor="white")
 
@@ -170,14 +183,15 @@ if __name__ == "__main__":
                 m += 1.0
 
             ax.legend()
-            ax.set_xticks(x, collected.planner.data, rotation=90)
+            ax.set_xticks(x, collected.planner.data, rotation=45)
             ax.tick_params(axis='y', labelsize=10)
             ax.tick_params(axis='x', labelsize=10)
             ax.set_ylabel(f"{metric.replace('_', ' ').title()}", fontsize=12)
             plt.show()
 
         # TODO: Save the results of each benchmark into a PDF file with the name of the experiment on it (experiment+scene_name), instead of just displaying them
-        
-    IPython.embed()
+
+    if args.ishell:
+        IPython.embed()
 
 
